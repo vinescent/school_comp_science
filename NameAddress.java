b85f23a55ba1ac87e0ca9ea9ba3f18a4c8804cb2
vinescent <jshaurya21@gmail.com>
2026-03-02 10:05:01 -0500

    Uploaded first files

---
+++ NameAddress.java
@@ -0,0 +1,13 @@
+//Shaurya Jain
+//NameAddress
+// This program displays your name, street address, and city, st, zip.
+
+class NameAddress {
+  public static void main(String[] args) {
+    
+    System.out.println("Bob Jones");
+    System.out.println("123 Ave");
+    System.out.println("New York, NY 12345");
+
+  }
+}
